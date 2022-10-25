cffaafb5aaa08851d9033d58521c8d0773aa0b75
Tarcísio Delmondes <tarcisiodelmondes@gmail.com>
2022-10-25 19:20:50 -0300

    Add typeorm config

---
+++ src/shared/index.ts
@@ -0,0 +1,33 @@
+import { resolve } from "node:path";
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database:
+    process.env.NODE_ENV === "test" ? process.env.DATABASE_NAME : "rentx",
+  migrations: [resolve(__dirname, "migrations", "{*.js,*.ts}")],
+  entities: [
+    resolve(
+      __dirname,
+      "..",
+      "..",
+      "..",
+      "modules",
+      "*",
+      "infra",
+      "typeorm",
+      "entities",
+      "{*.js,*.ts}"
+    ),
+  ],
+});
+
+AppDataSource.initialize()
+  .then(() => console.log("The connection to the database was successful"))
+  .catch((err) => {
+    throw new Error(`Database connection failed, error: ${err}`);
+  });
